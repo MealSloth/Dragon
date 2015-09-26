a3c838a1bcbf5cb28ee4cc147b9f5a021793affe
Michael Fourre <michael@michaels-macbook-air.local>
2015-09-25 20:06:16 -0400

    Initial Commit

---
+++ Dragon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dragon
+//
+//  Created by Michael Fourre on 9/25/15.
+//  Copyright © 2015 MealSloth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
