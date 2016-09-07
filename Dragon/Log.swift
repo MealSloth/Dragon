af26435fad4325e6b81c7c321d9c58e75e3a9298
Michael <michael@mealsloth.com>
2016-09-07 15:26:18 -0700

    DRAGON-65 progress

--- Dragon/Log.swift
+++ Dragon/Log.swift
@@ -20,56 +20,39 @@ class Log
         case warning = "Warning"
     }
     
-    /* Objects which do not implement protocol CustomStringConvertible will need to be handled manually
-       with the data parameter. A commented example is given below showing how this would work. */
-    internal class func log(type type: LogType, string: String, data: Dictionary<String, String?> = [:])
+    internal class func log(type type: LogType, string: String?)
     {
-        var log = "Log.\(type.rawValue): \(string)"
-        for (key, value) in data
-        {
-            let val = (value == nil) ? "" : value!
-            log = log.stringByReplacingOccurrencesOfString(key, withString: val)
-        }
+        let log = "Log.\(type.rawValue): \(string)"
         print(log)
     }
     
-    class func Debug(string: String)
+    class func Debug(string: String?)
     {
         self.log(type: .debug, string: string)
     }
     
-    class func Error(string: String)
+    class func Error(string: String?)
     {
         self.log(type: .error, string: string)
     }
     
-    class func Info(string: String)
+    class func Info(string: String?)
     {
         self.log(type: .info, string: string)
     }
     
-    class func Normal(string: String)
+    class func Normal(string: String?)
     {
         self.log(type: .normal, string: string)
     }
     
-    class func Verbose(string: String)
+    class func Verbose(string: String?)
     {
         self.log(type: .verbose, string: string)
     }
     
-    class func Warning(string: String)
+    class func Warning(string: String?)
     {
         self.log(type: .warning, string: string)
     }
-    
-//    class func string(string: String, type: LogType = .info)
-//    {
-//        let str = ":str"
-//        
-//        var data: Dictionary<String, String> = [:]
-//        data[str] = string
-//        
-//        self.log(type: type, string: "\(str)", data: data)
-//    }
 }
--- Dragon/Model.swift
+++ Dragon/Model.swift
@@ -1,14 +1,15 @@
 //
-//  UserModel.swift
+//  Model.swift
 //  Dragon
 //
-//  Created by Michael Fourre on 4/29/16.
+//  Created by Michael Fourre on 9/7/16.
 //  Copyright © 2016 MealSloth. All rights reserved.
 //
 
 import Foundation
+import CoreData
 
-class UserModel: Model
+class Model: NSManagedObject
 {
     
 }
