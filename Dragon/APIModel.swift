1de2ed345b0836bf112158911fe4de1232ab63f0
Michael <michael@mealsloth.com>
2016-09-15 14:26:39 -0700

    This file was in conflict

--- Dragon/APIModel.swift
+++ Dragon/APIModel.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class APIModel: NSObject
+class APIModel: NSObject, PrettyPrintable
 {
     override var description: String {
         let string = self.getPropertiesString(self)
@@ -21,6 +21,11 @@ class APIModel: NSObject
         self.initialize(json)
     }
     
+    override func valueForKey(key: String) -> AnyObject?
+    {
+        return super.valueForKey(key)
+    }
+    
     func initialize(json: Dictionary<String, AnyObject>, skip: [String] = [])
     {
         for property in self.getProperties()
@@ -30,59 +35,6 @@ class APIModel: NSObject
                 self.setValue(json[FieldNameHelper.GetServerName(forClientName: property)], forKey: property)
             }
         }
-    }
-    
-    func getProperties() -> [String]
-    {
-        return Mirror(reflecting: self).children.filter { $0.label != nil }.map { $0.label! }
-    }
-    
-    private func getPropertiesString(context: APIModel, depth: Int = 0) -> String
-    {
-        var string: String = (depth == 0) ? "\n" : ""
-        string += "\(Mirror(reflecting: self).subjectType): {\n"
-        for property in context.getProperties()
-        {
-            string += context.getPropertyString(context, property: property, depth: depth + 1)
-        }
-        for _ in 0..<depth
-        {
-            string += "    "
-        }
-        string += "}\n"
-        return string
-    }
-    
-    private func getPropertyString(context: APIModel, property: String, string s: String = "", depth d: Int = 0) -> String
-    {
-        let depth: Int = d
-        var string: String = s
-        for _ in 0..<depth
-        {
-            string += "    "
-        }
-        if let newProp = context.valueForKey(property) as? APIModel
-        {
-            string += "\(property):\n\(self.getPropertiesString(newProp, depth: depth))"
-        }
-        else if let arr = context.valueForKey(property) as? [APIModel]
-        {
-            if arr.count > 0
-            {
-                for model in arr
-                {
-                    string += model.getPropertiesString(model)
-                }
-            }
-            else
-            {
-                string += "\(property): {}\n"
-            }
-        }
-        else
-        {
-            string += "\(property): \(self.valueForKey(property))\n"
-        }
-        return string
+        Log.Info("Finished initializing model with values:\n\(self.description)")
     }
 }
