1659625d1db3b6a5838d35298a649d2287d2fcde
Michael <michael@mealsloth.com>
2016-11-05 11:13:48 -0700

    Switching Model classes instead of APIModel classes

--- Dragon/User.swift
+++ Dragon/User.swift
@@ -11,12 +11,6 @@ import Foundation
 class User: Model
 {
     @NSManaged var id: String!
-    @NSManaged var userLoginID: String!
-    @NSManaged var consumerID: String!
-    @NSManaged var chefID: String!
-    @NSManaged var locationID: String!
-    @NSManaged var billingID: String!
-    @NSManaged var profilePhotoID: String!
     @NSManaged var email: String!
     @NSManaged var firstName: String?
     @NSManaged var lastName: String?
--- Dragon/UserAPIModel.swift
+++ Dragon/UserAPIModel.swift
@@ -11,6 +11,12 @@ import Foundation
 class UserAPIModel: APIModel
 {
     var id: String!
+    var userLoginID: String!
+    var consumerID: String!
+    var chefID: String!
+    var locationID: String!
+    var billingID: String!
+    var profilePhotoID: String!
     var email: String!
     var firstName: String?
     var lastName: String?
