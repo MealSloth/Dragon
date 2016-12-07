385b82f3fdaca1cb0f600b8c00e484a61288a12f
Michael <michael@mealsloth.com>
2016-12-06 23:22:15 -0800

    Recursive initialization of models and arrays of models in APIModel generic initializer helper

--- Dragon/APIModel.swift
+++ Dragon/APIModel.swift
@@ -14,7 +14,7 @@ class APIModel: NSObject, PrettyPrintable
         return self.getPropertiesString(self)
     }
     
-    init(json: [String: Any])
+    required init(json: [String: Any])
     {
         super.init()
         self.initialize(json)
@@ -39,9 +39,45 @@ class APIModel: NSObject, PrettyPrintable
         {
             if !skip.contains(property)
             {
-                self.setValue(json[T.getServerName(forClientName: property)], forKey: property)
+                let value = self.value(forKey: property)
+                if let model = value as? APIModel //Recursively handle models
+                {
+                    let type = type(of: model)
+                    if let model = getModel(model: json[T.getServerName(forClientName: property)] as? [String: Any], using: type)
+                    {
+                        self.setValue(model, forKey: property) //Finally, set the value
+                        continue //Onto the next property
+                    }
+                }
+                else if let models = value as? [APIModel] //Recursively handle arrays of models
+                {
+                    let type = type(of: models).Element.self
+                    if let modelArray = getArrayOfModels(array: json[T.getServerName(forClientName: property)] as? [[String: Any]], using: type)
+                    {
+                        self.setValue(modelArray, forKey: property) //Finally, set the value
+                        continue //Onto the next property
+                    }
+                }
+                self.setValue(json[T.getServerName(forClientName: property)], forKey: property) //Run only if no `continue`
             }
         }
         Log.info("Finished initializing model with values: \(self.description)")
+    }
+    
+    func getModel<T: APIModel>(model: [String: Any]?, using: T.Type?) -> T?
+    {
+        guard let apiModel = model else { return nil }
+        return T(json: apiModel)
+    }
+    
+    func getArrayOfModels<T: APIModel>(array: [[String: Any]]?, using: T.Type?) -> [T]?
+    {
+        guard let modelArray = array else { return nil }
+        var models: [T] = []
+        for i in 0..<modelArray.count
+        {
+            models.append(T(json: modelArray[i])) //Recursively initialize nested Models
+        }
+        return models
     }
 }
