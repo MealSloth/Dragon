5f95f50a5b673de909e69f086535f05d5ba0f41f
Michael <michael@mealsloth.com>
2016-12-13 09:43:41 -0800

    Finished adding test API requests and test models for json parsing testing

--- Dragon/TestAPIModel.swift
+++ Dragon/TestAPIModel.swift
@@ -30,7 +30,82 @@ class TestAPIModel: APIModel
 //    
 //    required init?(jsonOptional: [String: Any]?)
 //    {
-//        super.init(jsonOptional: jsonOptional)
+//        guard let json = jsonOptional else { return nil }
+//        
+//        super.init()
+//        
+//        self.testInt = json[fieldTestInt] as? NSNumber
+//        self.testString = json[fieldTestString] as? String
+//        
+//        let intList = json[fieldTestIntList] as? [Int]
+//        var list: [NSNumber] = []
+//        if let realIntList = intList
+//        {
+//            for i in realIntList
+//            {
+//                list.append(i as NSNumber)
+//            }
+//        }
+//        self.testIntList = list
+//        
+//        let stringList = json[fieldTestStringList] as? [String]
+//        var list2: [String] = []
+//        if let realStringList = stringList
+//        {
+//            for i2 in realStringList
+//            {
+//                list2.append(i2)
+//            }
+//        }
+//        self.testStringList = list2
+//        
+//        if let postJson = json[fieldTestPostModel] as? [String: Any]
+//        {
+//            self.testPostModel = PostAPIModel(json: postJson)
+//        }
+//        
+//        if let consumerJson = json[fieldTestConsumerModel] as? [String: Any]
+//        {
+//            self.testConsumerModel = ConsumerAPIModel(json: consumerJson)
+//        }
+//        
+//        if let userList = json[fieldTestUserModelList] as? [Any]
+//        {
+//            var userModelList: [UserAPIModel] = []
+//            for user in userList
+//            {
+//                if let userModel = user as? [String: Any]
+//                {
+//                    userModelList.append(UserAPIModel(json: userModel))
+//                }
+//            }
+//            self.testUserModelList = userModelList
+//        }
+//        
+//        if let consumerList = json[fieldTestConsumerModelList] as? [Any]
+//        {
+//            var consumerModelList: [ConsumerAPIModel] = []
+//            for consumer in consumerList
+//            {
+//                if let consumerModel = consumer as? [String: Any]
+//                {
+//                    consumerModelList.append(ConsumerAPIModel(json: consumerModel))
+//                }
+//            }
+//            self.testConsumerModelList = consumerModelList
+//        }
+//        
+//        self.initialize(json, skip: [
+//            fieldTestInt,
+//            fieldTestString,
+//            fieldTestIntList,
+//            fieldTestStringList,
+//            fieldTestPostModel,
+//            fieldTestConsumerModel,
+//            fieldTestUserModelList,
+//            fieldTestConsumerModelList,
+//        ])
+//        
 //    }
 //    
 //    required init(json: [String: Any])
--- DragonTests/TestTest.swift
+++ DragonTests/TestTest.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class TestTest: DragonTest
 {
-    func testWithNothingLol()
+    func testTestLol()
     {
         let ready = expectation(description: "ready")
         let method = "TestRequest()"
