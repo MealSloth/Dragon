58f9cffb53d47a6e851013c4634d266918bd4335
Michael <michael@mealsloth.com>
2016-12-20 22:30:34 -0800

    Reducing redundant try blocks in Insertable protocol

--- Dragon/Insertable.swift
+++ Dragon/Insertable.swift
@@ -35,46 +35,30 @@ extension Insertable where Self: NSManagedObject
         let tableName = table ?? self.entityName
         let request: NSFetchRequest<Self> = NSFetchRequest(entityName: tableName)
         request.predicate = NSPredicate(format: "id == %@", id)
-        do
+        if let object = try? context.fetch(request)[safe: 0]
         {
-            let result = try context.fetch(request)
-            if let object = result[safe: 0]
+            return object
+        }
+        else
+        {
+            if let entity = NSEntityDescription.entity(forEntityName: tableName, in: context)
             {
-                return object
+                return self.init(entity: entity, insertInto: context)
             }
             else
             {
-                if let entity = NSEntityDescription.entity(forEntityName: tableName, in: context)
-                {
-                    return self.init(entity: entity, insertInto: context)
-                }
-                else
-                {
-                    Log.error("No entity found with name: \(tableName)")
-                }
+                Log.error("No entity found with name: \(tableName)")
             }
-        }
-        catch let error
-        {
-            Log.error("Error during fetch request: \(error)")
         }
         return nil
     }
     
     static func insert(_ model: APIModel?, into table: String?) -> Self?
     {
-        guard let model = model, let object = Self.insert(at: model.value(forKey: "id") as? String, into: table) else { return nil }
+        guard let object = Self.insert(at: model?.value(forKey: "id") as? String, into: table) else { return nil }
         object.populate(using: model, skip: [])
-        do
-        {
-            try self.context?.save()
-            return object
-        }
-        catch let error
-        {
-            Log.error("Error during context save: \(error)")
-            return nil
-        }
+        try? self.context?.save()
+        return object
     }
     
     static func insert(_ model: APIModel?) -> Self?
