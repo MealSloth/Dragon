c301d33d975ca5edac55aa29664a56b0cd2e125c
Michael <michael@mealsloth.com>
2017-02-25 14:31:04 -0800

    Include an (albeit redundant) reference to the NSManagedObjectContext member necessary for any Manageable adopters

--- Dragon/Manageable.swift
+++ Dragon/Manageable.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2016 MealSloth. All rights reserved.
 //
 
+import CoreData
+
 //
 //  Manageable is simply a collective protocol which implements all available DB protocols
 //
+
 protocol Manageable: Deletable, Fetchable, Insertable, Updatable {
-    
+    static var context: NSManagedObjectContext? { get }
 }
